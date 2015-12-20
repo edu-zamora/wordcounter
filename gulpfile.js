fbca33c7e67790cfbddfbbed47776114d11d363b
Fengyuan Chen <cfy.org@gmail.com>
2015-12-20 13:23:52 +0800

    Update tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,120 +1,89 @@
-(function () {
+'use strict';
 
-  'use strict';
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var pkg = require('./package');
+var scripts = {
+      all: [
+        'gulpfile.js',
+        'src/wordcounter.js',
+        'demo/js/main.js',
+        'test/*.js'
+      ],
+      demo: 'demo/js',
+      min: 'wordcounter.min.js',
+      src: 'src/wordcounter.js',
+      dest: 'dist'
+    };
+var replacement = {
+      regexp: /@\w+/g,
+      filter: function (placeholder) {
+        switch (placeholder) {
+          case '@VERSION':
+            placeholder = pkg.version;
+            break;
 
-  var gulp = require('gulp'),
-      jshint = require('gulp-jshint'),
-      jscs = require('gulp-jscs'),
-      rename = require('gulp-rename'),
-      uglify = require('gulp-uglify'),
-      replace = require('gulp-replace'),
-      csslint = require('gulp-csslint'),
-      cssmin = require('gulp-cssmin'),
-      htmlmin = require('gulp-htmlmin'),
-      nodeunit = require('gulp-nodeunit'),
-      del = require('del'),
-      fs = require('fs'),
-      pkg = require('./package'),
-      jshintOptions = JSON.parse(fs.readFileSync('.jshintrc')),
-      jscsOptions = JSON.parse(fs.readFileSync('.jscsrc')),
-      csslintOptions = JSON.parse(fs.readFileSync('.csslintrc'));
+          case '@YEAR':
+            placeholder = (new Date()).getFullYear();
+            break;
 
-  gulp.task('clean', function (cb) {
-    return del(['dist', '_gh_pages'], cb);
-  });
+          case '@DATE':
+            placeholder = (new Date()).toISOString();
+            break;
+        }
 
-  gulp.task('check', function () {
-    return gulp.src(['*.js', 'test/*.js', 'src/*.js', 'docs/js/docs.js'])
-    .pipe(jshint(jshintOptions))
-    .pipe(jshint.reporter('default'))
-    .pipe(jscs(jscsOptions));
-  });
-
-  gulp.task('test', ['check'], function () {
-    return gulp.src('test/*.js')
-    .pipe(nodeunit());
-  });
-
-  gulp.task('copy', ['clean'], function () {
-    return gulp.src('src/*.js')
-    .pipe(replace(/@\w+/g, function (placeholder) {
-      switch (placeholder) {
-        case '@VERSION':
-          placeholder = pkg.version;
-          break;
-
-        case '@YEAR':
-          placeholder = (new Date()).getFullYear();
-          break;
-
-        case '@DATE':
-          placeholder = (new Date()).toISOString();
-          break;
+        return placeholder;
       }
+    };
 
-      return placeholder;
+gulp.task('jscopy', function () {
+  return gulp.src(scripts.src)
+    .pipe(gulp.dest(scripts.demo))
+    .pipe(gulp.dest(scripts.dest));
+});
+
+gulp.task('jshint', function () {
+  return gulp.src(scripts.all)
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter('default'));
+});
+
+gulp.task('jscs', function () {
+  return gulp.src(scripts.all)
+    .pipe(plugins.jscs())
+    .pipe(plugins.jscs.reporter());
+});
+
+gulp.task('js', ['jshint', 'jscs'], function () {
+  return gulp.src(scripts.src)
+    .pipe(plugins.replace(replacement.regexp, replacement.filter))
+    .pipe(gulp.dest(scripts.demo))
+    .pipe(gulp.dest(scripts.dest))
+    .pipe(plugins.rename(scripts.min))
+    .pipe(plugins.uglify({
+      preserveComments: 'license'
     }))
-    .pipe(gulp.dest('dist'));
-  });
+    .pipe(gulp.dest(scripts.dest));
+});
 
-  gulp.task('compress', ['test', 'copy'], function () {
-    return gulp.src('dist/*.js')
-    .pipe(uglify({
-      preserveComments: 'some'
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('dist'));
-  });
+gulp.task('test', ['js'], function () {
+  return gulp.src('test/*.js')
+  .pipe(plugins.mocha());
+});
 
-  gulp.task('release', ['compress'], function () {
-    return gulp.src('dist/*.js')
-    .pipe(gulp.dest('releases/' + pkg.version));
-  });
-
-  gulp.task('sync', ['compress'], function () {
-    return gulp.src('dist/*.js')
-    .pipe(gulp.dest('docs/js'))
-    .pipe(gulp.dest('_gh_pages/js'));
-  });
-
-  gulp.task('docs.img', ['clean'], function () {
-    return gulp.src('docs/img/*')
-    .pipe(gulp.dest('_gh_pages/img'));
-  });
-
-  gulp.task('docs.css', ['clean'], function () {
-    return gulp.src('docs/css/docs.css')
-    // .pipe(csslint('.csslintrc'))
-    .pipe(csslint(csslintOptions))
-    .pipe(csslint.reporter())
-    .pipe(cssmin())
-    .pipe(gulp.dest('_gh_pages/css'));
-  });
-
-  gulp.task('docs.js', ['clean'], function () {
-    return gulp.src('docs/js/docs.js')
-    .pipe(jshint(jshintOptions))
-    .pipe(jshint.reporter())
-    .pipe(uglify())
-    .pipe(gulp.dest('_gh_pages/js'));
-  });
-
-  gulp.task('docs.misc', ['clean'], function () {
-    return gulp.src('docs/js/jquery.js')
-    .pipe(gulp.dest('_gh_pages/js'));
-  });
-
-  gulp.task('docs', ['docs.img', 'docs.css', 'docs.js', 'docs.misc'], function () {
-    return gulp.src('docs/*.html')
-    .pipe(htmlmin({
-      collapseWhitespace: true,
-      collapseBooleanAttributes: true
-    }))
+gulp.task('site', function () {
+  return gulp.src('demo/**')
     .pipe(gulp.dest('_gh_pages'));
-  });
+});
 
-  gulp.task('default', ['sync', 'docs']);
+gulp.task('release', ['test', 'site'], function () {
+  return gulp.src('dist/*.js')
+    .pipe(gulp.dest('_gh_pages/js'))
+    .pipe(gulp.dest('_releases/' + pkg.version));
+});
 
-})();
+gulp.task('watch', function () {
+  gulp.watch(scripts.src, ['jscopy']);
+});
+
+gulp.task('default', ['watch']);
